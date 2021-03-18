23d585ac8f8eead25c5ea4b5bbb6297bc03e0ca1
Robert Sampang <maravilloroan@gmail.com>
2021-03-18 19:05:17 +0800

    Responsive Landing Page

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector('.menu');
+const nav = document.querySelector('nav');
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    menuBtn.classList.toggle('active');
+});
+
